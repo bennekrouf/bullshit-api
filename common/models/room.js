0b8a61665cb4f804f4badfc74ae6b3ea4c6e83e0
bennekrouf <mohamed.bennekrouf@b-i.com>
2016-05-30 20:47:58 +0200

    generate combination

--- common/models/room.js
+++ common/models/room.js
@@ -1,3 +1,43 @@
-module.exports = function(Room) {
+'use strict mode'
 
-};
+module.exports = function(Room) {
+  var app = require('../../server/server');
+
+  Room.remoteMethod(
+    	'game',
+    	{
+        http: {path: '/game', verb: 'get'},
+        returns: {arg: 'game', type: 'Object'}
+    	})
+
+  Room.game = function(cb) {
+
+    "use strict"
+    const nbr = 3
+    let combinaison = {
+      nbr: nbr
+    }
+
+    var Bullshit = app.models.Bullshit;
+
+    Bullshit.find(function(err, bullshits) {
+      bullshits = bullshits.map(function(bullshit){
+        return bullshit.word
+      })
+      let rand, line = [], column
+      for(let indexligne=0; indexligne<combinaison.nbr; indexligne++){
+        column = []
+        for(let indexcolumn=0; indexcolumn<combinaison.nbr; indexcolumn++){
+          rand = bullshits[Math.floor(Math.random() * bullshits.length)];
+          bullshits = bullshits.filter(function(bullshit){
+            return bullshit !== rand
+          })
+          column.push(rand)
+        }
+        line.push(column)
+      }
+      combinaison.value = line
+      cb(null, combinaison)
+    })
+  }
+}
